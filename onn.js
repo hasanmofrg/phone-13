2ad0399e266c0b3635f0999961f71c4ec14e4c69
Hasan mofrg <mjhasan130@gmail.com>
2024-10-30 01:06:58 +0300

    Add files via upload

---
+++ onn.js
@@ -0,0 +1,11 @@
+function myFunction(smallimg) {
+    var fullimg = document.getElementById("img");
+    fullimg.src = smallimg.src
+}
+let hhh = document.querySelector(".all");
+
+function colors(color) {
+
+    hhh.style.background = color
+
+}
